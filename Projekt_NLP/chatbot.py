e06e5a99684582c560936adab058c7423a37eb4e
Joanna Walkiewicz <s20161@pjwstk.edu.pl>
2023-01-18 19:36:04 +0100

    add a new method: cleaning of the texts

--- Projekt_NLP/chatbot.py
+++ Projekt_NLP/chatbot.py
@@ -30,4 +30,31 @@ for conversation in conversations_ids:
         for i in range(len(conversation)-1):
             questions.append(id2line[conversation[i]])
             answers.append(id2line[conversation[i+1]])
-            
+
+#Doing a first cleaning of the texts
+def clean_text(text):
+    text = text.lower()
+    text = re.sub(r"i'm", "i am", text)
+    text = re.sub(r"he's", "he is", text)
+    text = re.sub(r"she's", "she is", text)
+    text = re.sub(r"that's", "that is", text)
+    text = re.sub(r"what's", "what is", text)
+    text = re.sub(r"where's", "where is", text)
+    text = re.sub(r"\'ll", " will", text)
+    text = re.sub(r"\'ve", " have", text)
+    text = re.sub(r"\'re", " are", text)
+    text = re.sub(r"\'d", " would", text)
+    text = re.sub(r"won't", "will not", text)
+    text = re.sub(r"can't", "cannot", text)
+    text = re.sub(r'[^a-zA-Z0-9\s]+', "", text)
+    return text
+
+#Cleaning the questions
+clean_questions = []
+for question in questions:
+    clean_questions.append(clean_text(question))
+    
+#Cleaning the answers
+clean_answers = []
+for answer in answers:
+    clean_answers.append(clean_text(answer))
